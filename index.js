651e932a276b35624188083602053c2027555c61
Ekulitech <woodynyam@gmail.com>
2025-05-13 02:57:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// images = new Array["RANDY/tester.jpg","RANDY/tester2.jpg","RANDY/worker.jpg"];
+//var randomim = Math.floor(Math.random() * images.length);
+//document.getElementById("im").src = images[randomim];
+
+
+    
+
+function get(color) {
+    var color = new Array["white","blue","grey"];
+//var images = new Array["RANDY/tester.jpg","RANDY/tester2.jpg","RANDY/worker.jpg"];
+var randomim = Math.floor(Math.random() * color.length);
+ document.getElementsByClassName("logo").style.background = color[randomim];
+
+}
+get()
+var color = new Array["white","blue","grey"];
+//var images = new Array["RANDY/tester.jpg","RANDY/tester2.jpg","RANDY/worker.jpg"];
+var randomim = Math.floor(Math.random() * color.length);
+ document.getElementsByClassName("logo").style.background = color[randomim];
+
+
